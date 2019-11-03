d311c3bc4cc4744658ac2d55f12c350f434a4ae6
Will7461 <43720307+Will7461@users.noreply.github.com>
2019-11-03 18:07:33 +0100

    Añadido objeto passenger

--- docs/game.js
+++ docs/game.js
@@ -1,5 +1,6 @@
 import Train, * as train from './train.js'
 import Rail, * as rail from './Rail.js'
+import Passenger, * as passenger from './passenger.js'
 import {directionEnum, matrixEnum} from './Enums.js'
 
 const COLUMNS_CONST = 28;
@@ -23,6 +24,7 @@ export default class Game extends Phaser.Scene {
     this.load.image('plantillasprite', 'img/Plantilla.png', {frameWidth: 1400, frameHeight: 800})
     this.load.image('railsprite', 'img/rail.png', {frameWidth: 27, frameHeight: 40})
     this.load.image('trainsprite', 'img/trainwagon.png', { frameWidth: 50, frameHeight: 50 })
+    this.load.image('passengersprite', 'img/passenger.png', { frameWidth: 50, frameHeight: 50 })
   }
 
   create() 
@@ -34,6 +36,7 @@ export default class Game extends Phaser.Scene {
     this.plantilla = this.add.sprite(700, 400, 'plantillasprite');
     this.trainArray[0] = new Train(this, 14, 14, 'trainsprite', TRAIN_SPEED_CONST);
     this.trainArray[1] = new Train(this, 14, 15, 'trainsprite', TRAIN_SPEED_CONST);
+    this.passenger = new Passenger(this,10,10,'passengersprite');
 
     for(let i = 0; i < POOL_LENGTH_CONST; i++)
     {
---
+++ docs/passenger.js
@@ -0,0 +1,19 @@
+import {directionEnum, matrixEnum} from './Enums.js'
+
+export default class Rail extends Phaser.GameObjects.Sprite{
+    constructor(scene, column, row, texture)
+    {
+        super(scene, (column * 50) + 25, (row * 50) + 25, texture);
+        scene.add.existing(this);
+
+        this.column = column;
+        this.row = row;
+        this.direction = directionEnum.UP;
+        this.setDepth(1);
+    }
+    
+    preUpdate(){
+        this.column = Math.floor(this.x / 50);
+        this.row = Math.floor(this.y / 50);
+    }
+}
